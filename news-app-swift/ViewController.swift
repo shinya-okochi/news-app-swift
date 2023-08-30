a85ec02988357b58d8f5bdb705edca24129144e0
shinya-okochi <shny.kch@gmail.com>
2023-08-30 21:46:57 +0900

    プロジェクト作成

---
+++ news-app-swift/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  news-app-swift
+//
+//  Created by Shinya Okochi on 2023/08/30.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
